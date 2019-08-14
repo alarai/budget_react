bd1e1ce98ceeda22b38a2f7f6ac33b4f3b2a674c
alarai <wedge@atlanteans.net>
2019-08-14 15:32:12 +0200

    Change MEnu layoout to be center and use a dropdown for parameters to make it less congested

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -11,16 +11,19 @@ const NavBar = ({ user }) => {
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarsExample09"
-        aria-controls="navbarsExample09"
+        data-target="#navbar"
+        aria-controls="navbar"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon" />
       </button>
       {user && (
-        <div className="collapse navbar-collapse" id="navbarsExample09">
-          <ul className="navbar-nav mr-auto">
+        <div
+          className="collapse navbar-collapse justify-content-md-center"
+          id="navbar"
+        >
+          <ul className="navbar-nav">
             <li className="nav-item">
               <NavLink className="nav-item nav-link" to="/currents">
                 Current <span className="sr-only">(current)</span>
@@ -36,21 +39,36 @@ const NavBar = ({ user }) => {
                 Graphics
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-item nav-link" to="/recurings">
-                Recuring Operations
-              </NavLink>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="dropdown08"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Parameters
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdown08">
+                <li className="nav-item">
+                  <NavLink className="dropdown-item" to="/recurings">
+                    Recuring Operations
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="dropdown-item" to="/categories">
+                    Operation Categories
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="dropdown-item" to="/types">
+                    Operation Types
+                  </NavLink>
+                </li>
+              </div>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-item nav-link" to="/categories">
-                Operation Categories
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-item nav-link" to="/types">
-                Operation Types
-              </NavLink>
-            </li>
+
             <li className="nav-item">
               <NavLink className="nav-item nav-link" to="/logout">
                 Logout
